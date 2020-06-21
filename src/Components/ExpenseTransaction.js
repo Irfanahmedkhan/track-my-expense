ca534e4d9c78076fe0a531b0ac0aa04ed482d814
Irfanahmedkhan <64011793+Irfanahmedkhan@users.noreply.github.com>
2020-06-21 15:20:34 +0300

    update CSS (History)

--- src/Components/ExpenseTransaction.js
+++ src/Components/ExpenseTransaction.js
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../Context/Globalstate'
 
-const ExpenseTransaction = ({expensetransaction}) => {
+const ExpenseTransaction = ({ expensetransaction }) => {
 
     const { deletetransaction } = useContext(GlobalContext);
 
-    
+
     return (
 
         <li className='transaction'>
-            <span className='transaction-text'>{expensetransaction.expensetext}</span>
-            <span className='transaction-amount'>Rs.{expensetransaction.expenseamount}</span>
-            <button onClick={() => deletetransaction(expensetransaction.id)} className="delete-btn">x          
+
+            <span><button onClick={() => deletetransaction(expensetransaction.id)} className="edelete-btn">x
             </button>
+            </span> <span className='transaction-text'>{expensetransaction.expensetext}</span>
+            <span className='transaction-amount'>Rs.{expensetransaction.expenseamount}</span>
+
 
         </li>
     )
--- src/Components/IncomeTransaction.js
+++ src/Components/IncomeTransaction.js
@@ -1,18 +1,20 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { GlobalContext } from '../Context/Globalstate'
 
 
-const IncomeTransaction = ({incometransaction}) => {
-    
+const IncomeTransaction = ({ incometransaction }) => {
+
     const { deletetransaction } = useContext(GlobalContext);
 
     return (
-        
+
         <li className='transaction'>
+
+            <span>  <button onClick={() => deletetransaction(incometransaction.id)} className="idelete-btn">X
+            </button>
+            </span>
             <span className='transaction-text'>{incometransaction.incometext}</span>
             <span className='transaction-amount'>Rs.{incometransaction.incomeamount}</span>
-            <button onClick={() => deletetransaction(incometransaction.id)} className="delete-btn">X
-            </button>
 
         </li>
     )
