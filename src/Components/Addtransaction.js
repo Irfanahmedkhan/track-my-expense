0ed7ae7039fe337e341ea1f386d55a6ed44cdc51
Irfanahmedkhan <64011793+Irfanahmedkhan@users.noreply.github.com>
2020-06-26 16:04:18 +0300

    Update Addtransaction.js

--- src/Components/Addtransaction.js
+++ src/Components/Addtransaction.js
@@ -21,98 +21,53 @@ const Addtransaction = () => {
     const onsubmitincome = (e) => {
         e.preventDefault();
 
-        if (incometext !== "") {
-            const newincometransaction = {
-                id: uuidv4(),
-                incometext,
-                incomeamount: incomeamount * 1,
-            };
-
-            addincome(newincometransaction);
-
-            setincome({
-                incometext: "",
-                incomeamount: 0,
-            })
         }
     }
 
-
-
-
-    const [expense, setexpense] = useState({
-        expensetext: "",
-        expenseamount: 0,
-    });
-
-    const { expensetext, expenseamount } = expense;
-
-    const onchangeexpense = (e) => {
-        setexpense({ ...expense, [e.target.name]: e.target.value });
-    };
-
-    const onsubmitexpense = (e) => {
-        e.preventDefault();
-
-        if (expensetext !== "") {
-            const newexpensetransaction = {
-                id: uuidv4(),
-                expensetext,
-                expenseamount: expenseamount * 1,
-            };
-
-            addexpense(newexpensetransaction);
-
-            setexpense({
-                expensetext: "",
-                expenseamount: 0,
-            })
-        }
-    }
 
 
     return (
 
         <div className='input-group'>
-                <form onSubmit={onsubmitincome}>
-                    <div className='income'>
-                        <input type='text'
-                            placeholder='Add Income...' autoComplete='off'
-                            name='incometext'
-                            value={incometext}
-                            onChange={onchangeincome}>
-                        </input>
-                        <br />
+            <form onSubmit={onsubmitincome}>
+                <div className='income'>
+                    <input type='text'
+                        placeholder='Add Income...' autoComplete='off'
+                        name='incometext'
+                        value={incometext}
+                        onChange={onchangeincome}>
+                    </input>
+                    <br />
 
-                        <input type='number'
-                            placeholder='Amount...' autoComplete='off'
-                            name='incomeamount'
-                            value={incomeamount}
-                            onChange={onchangeincome}>
-                        </input>
-                        <input type='submit' value='submit'></input>
-                    </div>
-                </form>
-                <form onSubmit={onsubmitexpense}>
-                    <div className='expense'>
+                    <input type='number'
+                        placeholder='Amount...' autoComplete='off'
+                        name='incomeamount'
+                        value={incomeamount}
+                        onChange={onchangeincome}>
+                    </input>
+                    <input type='submit' value='submit'></input>
+                </div>
+            </form>
+            <form onSubmit={onsubmitexpense}>
+                <div className='expense'>
 
-                        <input type='text'
-                            placeholder='Add Expense...' autoComplete='off'
-                            name='expensetext'
-                            value={expensetext}
-                            onChange={onchangeexpense}>
-                        </input>
-                        <br />
-                        <input type='number'
-                            placeholder='Amount...' autoComplete='off'
-                            name='expenseamount'
-                            value={expenseamount}
-                            onChange={onchangeexpense}>
-                        </input>
+                    <input type='text'
+                        placeholder='Add Expense...' autoComplete='off'
+                        name='expensetext'
+                        value={expensetext}
+                        onChange={onchangeexpense}>
+                    </input>
+                    <br />
+                    <input type='number'
+                        placeholder='Amount...' autoComplete='off'
+                        name='expenseamount'
+                        value={expenseamount}
+                        onChange={onchangeexpense}>
+                    </input>
 
-                        <input type='submit' value='submit'></input>
-                    </div>
-                </form>
+                    <input type='submit' value='submit'></input>
+                </div>
+            </form>
         </div>
     )
 }
